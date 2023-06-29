426526f915ff704361f8d535bae413d0e15b1180
Asad Ullah <devxasd@gmail.com>
2023-06-29 10:52:49 +0500

    Lecture 018

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('data'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 20, 199, 0),
+              Color.fromARGB(255, 139, 219, 0),
+            ]),
+          ),
+          child: const Center(
+            child: Text('data'),
+          ),
         ),
       ),
     ),
